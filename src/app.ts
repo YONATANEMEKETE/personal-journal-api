185df391bac66d8d2df63570e3c38de3cb071607
YONATANEMEKETE <yonatanemekete33@gmail.com>
2026-06-08 10:50:08 +0300

    add helmet and cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,20 @@ import express from 'express';
 import { requestLogger } from './shared/middlewares/request-logger.js';
 import { notFound } from './shared/errors/error.js';
 import { errorHandler } from './shared/middlewares/error-handler.js';
+import helmet from 'helmet';
+import cors from 'cors';
 
 export const app = express();
 
 // NOTE: Middlewares
 app.use(requestLogger);
 app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
 
 app.get('/health', (_req, res) => {
   res.status(200).json({
