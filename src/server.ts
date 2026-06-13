bf4918a8bbf900b392a3b64c0613c290d89b42e4
YONATANEMEKETE <yonatanemekete33@gmail.com>
2026-06-13 19:42:06 +0300

    feat: implement server entry point with health check endpoints and graceful shutdown logic

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,69 @@
 import { app } from './app.js';
 import { configs } from './shared/configs/env.js';
+import { prisma } from './shared/db/prisma.js';
 import { logger } from './shared/utils/logger.js';
 
-app.listen(configs.PORT, () => {
+let isShuttingDown = false;
+
+// TODO: add startup validation like db-check, other required checks before server starts
+
+app.get('/health', (_req, res) => {
+  res.status(200).json({
+    status: 'ok',
+  });
+  logger.info('Server is healthy, and accepting HTTP requests');
+});
+
+app.get('/ready', (_req, res) => {
+  if (isShuttingDown) {
+    logger.info('Server is shutting down, and not accepting HTTP requests');
+    return res.status(503).json({
+      status: 'error',
+      code: 'SERVICE_UNAVAILABLE',
+      message: 'Server is shutting down',
+    });
+  }
+  res.status(200).json({
+    status: 'ok',
+  });
+  logger.info('Server is ready, and accepting HTTP requests');
+});
+
+const server = app.listen(configs.PORT, () => {
   logger.info(
     `Server is running on port http://localhost:${configs.PORT}, in ${configs.NODE_ENV} mode`,
   );
 });
+
+const gracefulShutdown = async () => {
+  logger.info('Received kill signal, shutting down gracefully.');
+  isShuttingDown = true;
+
+  logger.info('Closing HTTP server');
+  server.close(async (err) => {
+    if (err) {
+      logger.error('Error closing HTTP server: %s', err);
+      process.exit(1);
+    }
+    try {
+      logger.info('Closing database connections...');
+      await prisma.$disconnect();
+
+      logger.info('Graceful shutdown completed');
+      process.exit(0);
+    } catch (error) {
+      logger.error('Shutdown failed: %s', error);
+      process.exit(1);
+    }
+  });
+
+  setTimeout(() => {
+    logger.error(
+      'Could not close connections in time, forcefully shutting down.',
+    );
+    process.exit(1);
+  }, 30000);
+};
+
+process.on('SIGTERM', () => gracefulShutdown());
+process.on('SIGINT', () => gracefulShutdown());
